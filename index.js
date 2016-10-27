87f48ee6b78090145b414c5b7122ef16d56b0ac7
Maureen <mkbotoman@gmail.com>
2016-10-27 13:45:20 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="Hivemind Coffee Roasters" />
+    <meta name="keywords" content="Coffee,roaster,artisan" />
+  </head>
+  <body>
+
+  <div>
+    <h1>Hivemind Roasters</h1>
+
+    <footer>Copyright © Hivemind Roasters</footer>
+  </div>
+
+  </body>
+</html>
